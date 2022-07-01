206bd4feae3c1b07a11e7b80014c90534fcda743
dixitwappnet <dixit.wappnet@gmail.com>
2022-07-01 19:46:21 +0530

    initial commit

---
+++ src/api/consumer-homepage/routes/consumer-homepage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * consumer-homepage router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::consumer-homepage.consumer-homepage');
---
+++ src/api/consumer-homepage/services/consumer-homepage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * consumer-homepage service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::consumer-homepage.consumer-homepage');
