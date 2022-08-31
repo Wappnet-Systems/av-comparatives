9ecc501ed0bfe8df55eae3d1e863a8490706b031
dixitwappnet <dixit.wappnet@gmail.com>
2022-08-31 15:54:10 +0530

    contact us mail send

---
+++ src/api/send-press-contact-mail/controllers/send-press-contact-mail.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/**
+ * A set of functions called "actions" for `send-press-contact-mail`
+ */
+
+module.exports = {
+  sendMail: async (ctx, next) => {
+    try {
+      const { subject, message, name, email, role } = ctx.request.body;
+
+      let send = await strapi.plugins["email"].services.email.send({
+        to: process.env.CONTACT_TO_EMAIL_ADDRESS,
+        subject: subject,
+        replyTo: email,
+        text: `
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Role: ${role}</p>
+        <p>Message: ${message}</p>
+        `,
+        html: `
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Role: ${role}</p>
+        <p>Message: ${message}</p>
+        `,
+      });
+
+      ctx.body = {
+        error: false,
+        message: "Press contact mail sent successfully.",
+      };
+    } catch (err) {
+      ctx.body = {
+        error: true,
+        message: "Press contact mail sending failed.",
+      };
+    }
+  },
+};
