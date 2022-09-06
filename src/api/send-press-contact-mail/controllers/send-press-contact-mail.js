dfdc26949526d1a612a9cd0817bba5f8b767855a
dixitwappnet <dixit.wappnet@gmail.com>
2022-09-06 11:45:12 +0530

    press contact changes

--- src/api/send-press-contact-mail/controllers/send-press-contact-mail.js
+++ src/api/send-press-contact-mail/controllers/send-press-contact-mail.js
@@ -7,7 +7,7 @@
 module.exports = {
   sendMail: async (ctx, next) => {
     try {
-      const { subject, message, name, email, role } = ctx.request.body;
+      const { subject, message, name, email, publication, company_url } = ctx.request.body;
 
       let send = await strapi.plugins["email"].services.email.send({
         to: process.env.CONTACT_TO_EMAIL_ADDRESS,
@@ -16,13 +16,15 @@ module.exports = {
         text: `
         <p>Name: ${name}</p>
         <p>Email: ${email}</p>
-        <p>Role: ${role}</p>
+        <p>Publication: ${publication}</p>
+        <p>Company Url: ${company_url}</p>
         <p>Message: ${message}</p>
         `,
         html: `
         <p>Name: ${name}</p>
         <p>Email: ${email}</p>
-        <p>Role: ${role}</p>
+        <p>Publication: ${publication}</p>
+        <p>Company Url: ${company_url}</p>
         <p>Message: ${message}</p>
         `,
       });
